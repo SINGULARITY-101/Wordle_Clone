f8969c500a3e7f553b7ebdcfe80f64dcfd741592
Vishesh Kwatra <kwatra.vishesh@gmail.com>
2024-02-09 16:19:18 +0530

    Deleted the new lines from new_main

--- project_directory/new_main.py
+++
@@ -1,5 +0,0 @@
-print("This is a new file on a new branch with some changes that I am addding to get some")
-
-print("Another person was editing this file on his machine and submitted a pull request which got merged on this branch")
-
-print("Adding more stuff")
